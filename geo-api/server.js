0da618407a11573bc40ac8cddc9f38242e80f518
Timothée Serck <timserck@gmail.com>
2025-10-03 13:35:23 +0200

    resolve cors

--- geo-api/server.js
+++ geo-api/server.js
@@ -9,7 +9,7 @@ const PORT = 8443;
 app.use(cors());
 
 // (Optional, production: restrict to your domain only)
-// app.use(cors({ origin: "https://timserck.duckdns.org" }));
+app.use(cors({ origin: "https://timserck.duckdns.org" }));
 
 // Fallback: get coordinates from GeoJS
 const getCoords = async () => {
